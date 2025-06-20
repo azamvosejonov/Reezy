48dcb9d3d813ca25acca6366877b74c88c10932c
kali <kali@kali.kali>
2025-06-20 19:20:47 +0500

    requestsdsdkksdfv

--- tasks.py
+++ tasks.py
@@ -1,6 +1,9 @@
-from celery import shared_task
+from celery_app import celery
 
-@shared_task
+@celery.task
+
+
+@celery.task
 def periodic_task():
     print("Periodic task is running...")
     # Add your processing logic here
