2b808a6b960e921f641525e73f247643749a8f0e
kali <kali@kali.kali>
2025-06-20 17:39:58 +0500

    requestsdsdkksdfv

--- celery_config.py
+++ celery_config.py
@@ -2,7 +2,7 @@ from celery import Celery
 import os
 
 # Create Celery app
-app = Celery('main.celery_app',
+app = Celery('celery_config.app',
              broker=os.getenv('CELERY_BROKER_URL', 'redis://redis:6379/0'),
              backend=os.getenv('CELERY_RESULT_BACKEND', 'redis://redis:6379/0'))
 
